1015a1b09863ee6afbe0255fad20c77459fbed2f
Pedro Minuca <pminuca@gmail.com>
2024-11-25 16:19:24 +0000

    feat: adicionado score

--- principal_alunos.py
+++ principal_alunos.py
@@ -1,5 +1,4 @@
 from pacman import *
-from utils import *
 import time
 
 def obtem_direecao(ponto1, ponto2):
@@ -8,9 +7,9 @@ def obtem_direecao(ponto1, ponto2):
     dir_y = math.sin(theta)
     return dir_x, dir_y
 
-
 def mover_pacman(estado_jogo, pos, angulo):
-    arranjar_posicao_pacman(estado_jogo)
+    t.up()
+    t.goto((estado_jogo['pacman']['objeto'].pos()))
     t.setheading(angulo)
     t.up()
     t.fd(TAMANHO_CELULA)
@@ -32,10 +31,8 @@ def pacman_esquerda(estado_jogo):
 def movimenta_pinky(estado_jogo):
     pass
 
-
 def calculate_distance(pos1, pos2):
     return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
-
 
 def movimenta_clyde(estado_jogo):
     scatter_distance_threshold = 3
@@ -44,7 +41,6 @@ def movimenta_clyde(estado_jogo):
     ghost_pos = estado_jogo['fantasmas'][CLYDE_OBJECT]['objeto'].pos()
     
     return 
-
 
 def movimenta_inky(estado_jogo):
     direcao_escolhida = random.choice(DIRECOES_POSSIVEIS)
@@ -61,8 +57,10 @@ def movimenta_blinky(estado_jogo):
     return direcao_escolhida
 
 def perdeu_jogo(estado_jogo):
-    pass
-
+    for i in range (3,7):
+        if ha_colisao(t1=estado_jogo['pacman']['objeto'], t2=estado_jogo['fantasmas'][i]['objeto'], collision_distance=20):
+            terminar_jogo(estado_jogo)   
+    
 def atualiza_pontos(estado_jogo):
     x = estado_jogo['pacman']['objeto'].xcor() 
     y = estado_jogo['pacman']['objeto'].ycor()
@@ -74,7 +72,6 @@ def atualiza_pontos(estado_jogo):
         estado_jogo['marcador'].goto(x + 10,y + 10)
         estado_jogo['marcador'].dot(2,'blue')
         update_board(estado_jogo)
-
 
 def atualiza_mapa(estado_jogo, x, y, elemento):
     index = offset((x,y))
@@ -91,7 +88,6 @@ def actualiza_posicao_pacman_fantasma(estado_jogo):
         y = ghost['objeto'].ycor()
         atualiza_mapa(estado_jogo, x, y, ghost_id)
 
-
 def guarda_jogo(estado_jogo):
     actualiza_posicao_pacman_fantasma(estado_jogo)
     mapa = estado_jogo['mapa']
@@ -104,29 +100,32 @@ def guarda_jogo(estado_jogo):
             ficheiro.write(linha + '\n')
 
 def carrega_jogo(estado_jogo, nome_ficheiro):
-    estado_jogo['mapa'] = carrega_mapa(nome_ficheiro)
-    
+    mapa_jogo = [] 
 
+    with open(nome_ficheiro, 'r') as ficheiro: 
+        for linha_ficheiro in ficheiro: 
+            linha = [int(num) for num in linha_ficheiro.strip().split(',') if num]
+            mapa_jogo.extend(linha) 
 
-
-
-
+    estado_jogo['mapa'] = mapa_jogo
+    pontos = mapa_jogo.count(7)
+    estado_jogo['score'] = pontos
 
 if __name__ == '__main__':
+    #dicionario com as funcoes de movimento dos jogadores
     funcoes_jogador = {'pacman_cima': pacman_cima, 'pacman_baixo': pacman_baixo, 'pacman_esquerda': pacman_esquerda, 'pacman_direita': pacman_direita, 'guarda_jogo' : guarda_jogo, 'carrega_jogo' : carrega_jogo}    
     funcoes_fantasmas = {BLINKY_OBJECT : movimenta_blinky, PINKY_OBJECT : movimenta_pinky, INKY_OBJECT : movimenta_inky, CLYDE_OBJECT : movimenta_clyde}
-
 
     nome_ficheiro = input('Pretende carregar um mapa (Enter para carregar o mapa default): ')
     if nome_ficheiro == '':
         nome_ficheiro = 'mapa_inicial.txt'
-    ##dicionario com as funcoes de movimento dos jogadores
     
     #funções de inicio do jogo
     estado_jogo = init_state()
-    carrega_jogo(estado_jogo, nome_ficheiro)    
+    carrega_jogo(estado_jogo, nome_ficheiro)
     setup(estado_jogo, True, funcoes_jogador,funcoes_fantasmas)
-    
+    update_board(estado_jogo)
+
     #inicia_jogo(estado_jogo)
     while not perdeu_jogo(estado_jogo):
         if estado_jogo['mapa'] is not None:
